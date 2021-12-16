e158d2700e68da52bbddfe71c87c66455d9fa1bb
wy-chan <79743156+wy-chan@users.noreply.github.com>
2021-12-17 01:00:44 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function reloadPage(){
+  window.location.reload();
+}
+
+function openMenu(){
+  document.getElementsByClassName("menu-content")[0].classList.remove("hidden-mobile");
+}
+function closeMenu(){
+  document.getElementsByClassName("menu-content")[0].classList.add("hidden-mobile");
+}
